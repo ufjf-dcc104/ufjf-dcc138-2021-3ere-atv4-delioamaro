087346924238354b08f686dc6a6804f833c8f954
Delio <delio.amaro@ice.ufjf.br>
2022-01-23 19:39:06 -0300

    adiciona lista de sprites na Cena

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,11 @@
 import Cena from "./Cena.js";
+import Sprite from "./Sprite.js";
 
 const canvas = document.querySelector("canvas");
 const cena1 = new Cena(canvas);
+const pc = new Sprite({color:"orange"});
+const en1 = new Sprite({x:160, w:50, color:"blue"});
+
+cena1.adicionar(pc);
+cena1.adicionar(en1);
 cena1.desenhar();
